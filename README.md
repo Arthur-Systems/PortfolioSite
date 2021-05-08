571db055540c5849791686ed652766e1d31d3de7
Arthur-Systems <arthur.wei50@gmail.com>
2021-05-07 19:01:50 -0700

    commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PortfolioSite
+<h3>About</h3>
+<p>This project is testing my skills in UI and UX design.</p>
+
+
---
+++ Scripts/scripts.ts
@@ -0,0 +1,20 @@
+function openNav(){
+   var x = document.getElementById("Spotify");
+   
+   if (x.style.display == "none"){
+       x.style.display = "block";
+   }else{
+       x.style.display = "none";
+   }
+}
+function closeNav(){
+    document.getElementById("Spotify").style.width = "0";
+}
+
+function arrowmove(){
+var x = document.getElementById("Arrows");
+
+x.style.transform
+
+}
+
