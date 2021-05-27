ab72855b337de86f8ac32c670ca578990624d992
Haichuan <arthur.wei50@gmail.com>
2021-05-26 19:04:44 -0700

    Local Node
    Installed all node dependencies on the local folder

---
+++ src/Scripts/three.js
@@ -0,0 +1,18 @@
+import '/src/Css/style.css'
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const renderer = new THREE.WebGLRenderer({
+
+    canvas: document.querySelector('#bg'),
+
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerHeight, window.innerWidth);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
