b5bd97eacbfcf4839ea7a0f228e2d38457f15f22
Haichuan <arthur.wei50@gmail.com>
2021-05-28 00:30:34 -0700

    Three Js Canvas Setup

--- src/Scripts/three.js
+++ src/Scripts/three.js
@@ -1,18 +1,29 @@
 import '/src/Css/style.css'
-
 import * as THREE from 'three';
+import { Material, RedFormat } from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+var size = document.querySelector('#splash');
+var w = size.offsetWidth;
+var h = size.offsetHeight;
+const camera = new THREE.PerspectiveCamera(75, w / h, 2, 1000);
+camera.position.setZ(40);
+camera.position.setX(30);
+camera.position.setY(20);
 const renderer = new THREE.WebGLRenderer({
 
     canvas: document.querySelector('#bg'),
-
 });
+renderer.setPixelRatio(500, 500);
+renderer.setSize(w, h);
+size.appendChild(renderer.domElement);
+const geomenty = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: false });
+const torus = new THREE.Mesh(geomenty, material);
+scene.add(torus);
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerHeight, window.innerWidth);
-camera.position.setZ(30);
-
-renderer.render(scene, camera);
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
