27dfa46b8efa41e6f4866a2529f6990acd964fdb
Haichuan <arthur.wei50@gmail.com>
2021-06-30 22:32:29 -0700

    Ai Assisted JS Cleanup

--- src/Scripts/Interactions.js
+++ src/Scripts/Interactions.js
@@ -1,82 +1,31 @@
-function closeSpot() {
-    document.getElementById("Spotify").style.width = "0";
-}
-
-function arrowmove() {
-    var x = document.getElementById("Arrows");
-    x.style.transform;
-}
-
-
-function arrowturn() {}
-
-function openSpot() {
-    var x = document.getElementById("Spotify");
-    if (x.style.display == "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
-
 function social() {
 
     var x = document.getElementById("header");
-    if (x.classList.contains("social")) {
-        x.classList.remove("social");
-    } else {
-        x.classList.add("social");
-    }
+    x.classList.toggle("social");
 
 }
 
-
 function Drop() {
     var x = document.getElementById("NavDrop");
-    if (x.classList.contains("drop")) {
-        x.classList.remove("drop");
-    } else {
-        x.classList.add("drop");
-    }
+    x.classList.toggle("drop");
 }
 
 function Socialdrop() {
     var x = document.getElementById("SocialDrop");
-    if (x.classList.contains("drop")) {
-        x.classList.remove("drop");
-    } else {
-        x.classList.add("drop");
-    }
+    x.classList.toggle("drop");
 }
 
 function Flip() {
     var x = document.getElementById("arrow");
     var y = document.getElementById("arrow1");
-
-    if (x.classList.contains("flip")) {
-        x.classList.remove("flip");
-    } else {
-        x.classList.add("flip");
-    }
-    if (y.classList.contains("flip")) {
-        y.classList.remove("flip");
-    } else {
-        y.classList.add("flip");
-    }
+    x.classList.toggle("flip");
+    y.classList.toggle("flip");
 }
 
 function NavFlip() {
     var x = document.getElementById("arrownav");
     var y = document.getElementById("arrownav1");
-    if (x.classList.contains("flip")) {
-        x.classList.remove("flip");
-    } else {
-        x.classList.add("flip");
-    }
+    x.classList.toggle("flip");
+    y.classList.toggle("flip");
 
-    if (y.classList.contains("flip")) {
-        y.classList.remove("flip");
-    } else {
-        y.classList.add("flip");
-    }
 }
